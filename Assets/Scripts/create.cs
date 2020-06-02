5c29694e1ca013ad879d3db8c8b517e6d3b525b9
SudhanshuSharma194 <62305320+SudhanshuSharma194@users.noreply.github.com>
2020-06-02 21:45:23 +0530

    Room and assets creation

---
+++ Assets/Scripts/create.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class create : MonoBehaviour
+{
+    public GameObject panel;
+    public Text length;
+    public Text breadth;
+    public Text height;
+    public GameObject dd;
+    public Text label;
+    public GameObject sofa;
+    public GameObject chair;
+
+    float rotspeed = 40;
+    int l,b,h;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        int.TryParse(length.text, out l );
+        int.TryParse(breadth.text, out b);
+        int.TryParse(height.text, out h);
+        gameObject.transform.localScale = new Vector3(l, b, h);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+    public void form()
+    {
+        panel.SetActive(false);
+        
+        gameObject.SetActive(true);
+        dd.SetActive(true);
+
+    }
+    public void placeobject()
+    {
+        if (label.text == "Sofa")
+            sofa.SetActive(true);
+        else if (label.text == "Chair")
+            chair.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/setScale.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class setScale : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+   
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("x"))
+        {
+            transform.localScale += new Vector3(1, 0, 0);
+        }
+        if (Input.GetKeyDown("y"))
+        {
+            transform.localScale += new Vector3(0, 1, 0);
+        }
+        if (Input.GetKeyDown("z"))
+        {
+            transform.localScale += new Vector3(0, 0, 1);
+        }
+        if (Input.GetKeyDown("c"))
+        {
+            transform.localScale -= new Vector3(1, 0, 0);
+        }
+        if (Input.GetKeyDown("u"))
+        {
+            transform.localScale -= new Vector3(0, 1, 0);
+        }
+        if (Input.GetKeyDown("v"))
+        {
+            transform.localScale -= new Vector3(0, 0, 1);
+        }
+    }
+    
+}
