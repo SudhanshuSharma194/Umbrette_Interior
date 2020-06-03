8f41b9ecd7101a025706f41eb63ce1a18f4e111c
Shivam949 <64895886+Shivam949@users.noreply.github.com>
2020-06-03 17:27:08 +0530

    Add Room Textures

---
+++ Assets/Scripts/Textures.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Textures : MonoBehaviour
+{
+    public Texture[] textures;
+    public int currentTexture;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            currentTexture++;
+            currentTexture %= textures.Length;
+            //Renderer.material.mainTexture = textures[currentTexture];
+            GetComponent<Renderer>().material.mainTexture = textures[currentTexture];
+        }
+    }
+}
