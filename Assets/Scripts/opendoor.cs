0c09537f8579f067cedba03f3ab44383a20bf86a
Shivam949 <64895886+Shivam949@users.noreply.github.com>
2020-06-05 17:48:00 +0530

    Add Interaction with table and door

---
+++ Assets/Scripts/opendoor.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class opendoor : MonoBehaviour
+{
+    private bool doorOpened;
+    private bool coroutineAllowed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        doorOpened = false;
+        coroutineAllowed = true;
+    }
+
+    private void OnMouseDown()
+    {
+        Invoke("RunCoroutine", 0f);
+    }
+    private void RunCoroutine()
+    {
+        StartCoroutine("OpenThatDoor");
+    }
+    
+    private IEnumerator OpenThatDoor()
+    {
+        coroutineAllowed = false;
+        if (!doorOpened)
+        {
+            for (float i = 0f; i <= 90f; i += 3f)
+            {
+                transform.localRotation = Quaternion.Euler(0f, 0f, -i);
+                yield return new WaitForSeconds(0f);
+            }
+            doorOpened = true;
+        }
+        else
+        {
+            for (float i = 90f; i >= 0f; i -= 2f)
+            {
+                transform.localRotation = Quaternion.Euler(0f, 0f, -i);
+                yield return new WaitForSeconds(0f);
+            }
+            doorOpened = false;
+        }
+        coroutineAllowed = true;
+    }
+    
+}
