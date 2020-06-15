9c88683499725f94d13d89ce3a8a0010b6abd6c1
Shivam949 <64895886+Shivam949@users.noreply.github.com>
2020-06-15 11:23:15 +0530

    Add Outline

---
+++ Assets/QuickOutline/Scripts/enable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enable : MonoBehaviour
+{
+    Outline script;
+    public GameObject obj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        script = obj.GetComponent<Outline>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnMouseDown()
+    {
+        script.enabled = true;
+    }
+    private void OnMouseUp()
+    {
+        script.enabled = false;
+    }
+}
